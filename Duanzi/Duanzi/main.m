f898c488f19c8949aa78a7e68ff7d9d706045a88
xuxiansen <2465670381@qq.com>
2014-11-13 11:20:28 +0800

    创建工程
    第一次提交代码

---
+++ Duanzi/Duanzi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Duanzi
+//
+//  Created by 徐先森 on 14-11-13.
+//  Copyright (c) 2014年 徐先森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
